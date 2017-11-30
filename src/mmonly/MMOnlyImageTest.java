fc45cf1c3a454c76343ad8688572d2f85e32612b
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-11-30 10:10:26 +0800

    添加反盗链破解

--- src/mmonly/MMOnlyImageTest.java
+++ src/mmonly/MMOnlyImageTest.java
@@ -18,7 +18,8 @@ import java.util.List;
  */
 public class MMOnlyImageTest {
 
-    private static String url = "http://www.mmonly.cc/mmtp/qcmn/63973.html";
+
+    private static String url = MMOnlyData.DATA[MMOnlyData.DATA.length - 1];
 
     private static String RAGEX_IMAGE = ".html";
 
@@ -38,43 +39,52 @@ public class MMOnlyImageTest {
         }*/
 
 
-
-getDownLoadImage();
-
-
+        getDownLoadImage();
 
 
         //System.out.println(document.body());
     }
 
-    public static void getDownLoadImage(){
+    public static void getDownLoadImage() {
 
-          Document document = null;
+
+        for (int i = 0; i < MMOnlyData.DATA.length - 1; i++) {
+
+            if (MMOnlyData.DATA[i].equals(url)) {
+
+                System.out.println("数据已经重复");
+
+                return;
+            }
+
+
+        }
+
+
+        Document document = null;
         try {
             document = Jsoup.connect(url).get();
 
-          List<ImageItem>  datas= getImage(document);
+            List<ImageItem> datas = getImage(document);
 
 
+            File file = new File(String.format("C:\\Users\\gavin\\Desktop\\晨读\\今日头条\\%s\\%s",
+                    TimeUtil.getTimeShort(), datas.get(0).getTitle()));
 
 
-            File file=new File(String.format("C:\\Users\\gavin\\Desktop\\晨读\\今日头条\\%s\\%s",
-                    TimeUtil.getTimeShort(),datas.get(0).getTitle()));
-
-
-            if (!file.exists()){
+            if (!file.exists()) {
 
                 file.mkdirs();
             }
 
-            List<String> images=new ArrayList<>(datas.size());
+            List<String> images = new ArrayList<>(datas.size());
 
             for (ImageItem data : datas) {
 
                 images.add(data.getSrc());
             }
 
-            DownLoadUtils.downLoadImageReferrer(images,file.getAbsolutePath(),"http://www.mmonly.cc");
+            DownLoadUtils.downLoadImageReferrer(images, file.getAbsolutePath(), "http://www.mmonly.cc");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,30 +95,28 @@ getDownLoadImage();
     }
 
 
-
-
     public static List<ImageItem> getImage(Document document) throws IOException {
 
         Elements divElements = document.select("div[class=pages]");
         Elements liElements = divElements.get(0).getElementsByTag("li");
         Element element = liElements.get(0);
         String page = element.text();
-        String contentpage=page.substring(1,page.length()-2);
-        int  mPage = Integer.valueOf(contentpage).intValue();
+        String contentpage = page.substring(1, page.length() - 2);
+        int mPage = Integer.valueOf(contentpage).intValue();
 
-        String  requsetUrl=url;
+        String requsetUrl = url;
 
-        List<ImageItem> imageItems=new ArrayList<>(mPage);
+        List<ImageItem> imageItems = new ArrayList<>(mPage);
         for (int i = 1; i <= mPage; i++) {
 
-            if (i>1){
+            if (i > 1) {
 
-                String newstr=String.format("_%d.html",i);
-                requsetUrl= url.replace(RAGEX_IMAGE,newstr);
-                imageItems.add( getImageItem(requsetUrl));
-            }else {
+                String newstr = String.format("_%d.html", i);
+                requsetUrl = url.replace(RAGEX_IMAGE, newstr);
+                imageItems.add(getImageItem(requsetUrl));
+            } else {
 
-                imageItems.add( getImageItem(requsetUrl));
+                imageItems.add(getImageItem(requsetUrl));
             }
 
         }
@@ -120,8 +128,6 @@ getDownLoadImage();
     private static ImageItem getImageItem(String url) throws IOException {
 
 
-
-
         Document document = Jsoup.connect(url).get();
 
 
@@ -130,16 +136,15 @@ getDownLoadImage();
         Element imgElement = divElements.get(0).getElementsByTag("img").get(0);
 
 
-        String imageUrl= imgElement.attr("src");
+        String imageUrl = imgElement.attr("src");
 
-        String title=imgElement.attr("alt");
+        String title = imgElement.attr("alt");
 
 
-        ImageItem imageItem=new ImageItem(imageUrl,title);
+        ImageItem imageItem = new ImageItem(imageUrl, title);
 
         return imageItem;
     }
-
 
 
 }
