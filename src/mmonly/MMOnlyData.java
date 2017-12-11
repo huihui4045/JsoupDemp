fe5f9e2cdbb6dc62ab986abd3927b00539d09d40
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-12-11 16:33:02 +0800

    Merge branch 'master' of https://github.com/huihui4045/JsoupDemp
    # Conflicts:
    #	src/mmonly/MMOnlyData.java

--- src/mmonly/MMOnlyData.java
+++ src/mmonly/MMOnlyData.java
@@ -5,19 +5,20 @@ package mmonly;
  */
 public class MMOnlyData {
 
-    public static String [] DATA={"http://www.mmonly.cc/mmtp/hgmn/210457.html","http://www.mmonly.cc/mmtp/qcmn/209280.html"
-    ,"http://www.mmonly.cc/mmtp/bjnmn/211982.html","http://www.mmonly.cc/mmtp/bjnmn/210640.html","http://www.mmonly.cc/mmtp/hgmn/210487.html",
-    "http://www.mmonly.cc/mmtp/hgmn/1276.html","http://www.mmonly.cc/mmtp/ctmn/187802.html","http://www.mmonly.cc/mmtp/ctmn/179371.html",
-    "http://www.mmonly.cc/mmtp/ctmn/168137.html","http://www.mmonly.cc/mmtp/qcmn/209433.html","http://www.mmonly.cc/mmtp/qcmn/95451.html",
-    "http://www.mmonly.cc/mmtp/qcmn/620.html","http://www.mmonly.cc/mmtp/ctmn/153218.html","http://www.mmonly.cc/mmtp/qcmn/30664.html",
-    "http://www.mmonly.cc/mmtp/ctmn/211805.html","http://www.mmonly.cc/mmtp/hgmn/204995.html","http://www.mmonly.cc/mmtp/hgmn/441.html",
-    "http://www.mmonly.cc/mmtp/wgmv/210412.html","http://www.mmonly.cc/mmtp/xgmn/212606.html","http://www.mmonly.cc/mmtp/qcmn/213755.html",
-    "http://www.mmonly.cc/mmtp/xgmn/213908.html","http://www.mmonly.cc/mmtp/swmn/213653.html","http://www.mmonly.cc/mmtp/wgmv/130662.html",
-    "http://www.mmonly.cc/ktmh/dmmn/136698.html","http://www.mmonly.cc/mmtp/wgmv/184792.html"};
-    "http://www.mmonly.cc/mmtp/wgmv/210412.html","http://www.mmonly.cc/mmtp/xgmn/212606.html","http://www.mmonly.cc/mmtp/xgmn/35700.html",
-    "http://www.mmonly.cc/mmtp/xgmn/5418.html","http://www.mmonly.cc/mmtp/xgmn/3161.html","http://www.mmonly.cc/mmtp/xgmn/4076.html",
-   "http://www.mmonly.cc/mmtp/xgmn/212622.html","http://www.mmonly.cc/mmtp/xgmn/212080.html","http://www.mmonly.cc/mmtp/hgmn/213663.html",
-            "http://www.mmonly.cc/mmtp/xgmn/136957.html","http://www.mmonly.cc/mmtp/hgmn/212577.html","http://www.mmonly.cc/mmtp/qcmn/5688.html",
-            "http://www.mmonly.cc/mmtp/xgmn/212611.html","http://www.mmonly.cc/mmtp/swmn/213658.html"
-            };
+    public static String[] DATA = {"http://www.mmonly.cc/mmtp/hgmn/210457.html", "http://www.mmonly.cc/mmtp/qcmn/209280.html"
+            , "http://www.mmonly.cc/mmtp/bjnmn/211982.html", "http://www.mmonly.cc/mmtp/bjnmn/210640.html", "http://www.mmonly.cc/mmtp/hgmn/210487.html",
+            "http://www.mmonly.cc/mmtp/hgmn/1276.html", "http://www.mmonly.cc/mmtp/ctmn/187802.html", "http://www.mmonly.cc/mmtp/ctmn/179371.html",
+            "http://www.mmonly.cc/mmtp/ctmn/168137.html", "http://www.mmonly.cc/mmtp/qcmn/209433.html", "http://www.mmonly.cc/mmtp/qcmn/95451.html",
+            "http://www.mmonly.cc/mmtp/qcmn/620.html", "http://www.mmonly.cc/mmtp/ctmn/153218.html", "http://www.mmonly.cc/mmtp/qcmn/30664.html",
+            "http://www.mmonly.cc/mmtp/ctmn/211805.html", "http://www.mmonly.cc/mmtp/hgmn/204995.html", "http://www.mmonly.cc/mmtp/hgmn/441.html",
+            "http://www.mmonly.cc/mmtp/wgmv/210412.html", "http://www.mmonly.cc/mmtp/xgmn/212606.html", "http://www.mmonly.cc/mmtp/qcmn/213755.html",
+            "http://www.mmonly.cc/mmtp/xgmn/213908.html", "http://www.mmonly.cc/mmtp/swmn/213653.html", "http://www.mmonly.cc/mmtp/wgmv/130662.html",
+            "http://www.mmonly.cc/ktmh/dmmn/136698.html", "http://www.mmonly.cc/mmtp/wgmv/184792.html",
+            "http://www.mmonly.cc/mmtp/wgmv/210412.html", "http://www.mmonly.cc/mmtp/xgmn/212606.html", "http://www.mmonly.cc/mmtp/xgmn/35700.html",
+            "http://www.mmonly.cc/mmtp/xgmn/5418.html", "http://www.mmonly.cc/mmtp/xgmn/3161.html", "http://www.mmonly.cc/mmtp/xgmn/4076.html",
+            "http://www.mmonly.cc/mmtp/xgmn/212622.html", "http://www.mmonly.cc/mmtp/xgmn/212080.html", "http://www.mmonly.cc/mmtp/hgmn/213663.html",
+            "http://www.mmonly.cc/mmtp/xgmn/136957.html", "http://www.mmonly.cc/mmtp/hgmn/212577.html", "http://www.mmonly.cc/mmtp/qcmn/5688.html",
+            "http://www.mmonly.cc/mmtp/xgmn/212611.html", "http://www.mmonly.cc/mmtp/swmn/213658.html","http://www.mmonly.cc/mmtp/hgmn/198665.html",
+            "http://www.mmonly.cc/mmtp/hgmn/4371.html","http://www.mmonly.cc/mmtp/hgmn/184850.html","http://www.mmonly.cc/mmtp/hgmn/75398.html","http://www.mmonly.cc/mmtp/hgmn/174804.html"
+    };
 }
