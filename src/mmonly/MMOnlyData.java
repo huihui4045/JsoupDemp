00f0badc9f118ae92cace83296066c563200d535
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-12-18 11:09:52 +0800

    Merge branch 'master' of https://github.com/huihui4045/JsoupDemp
    # Conflicts:
    #	src/mmonly/MMOnlyData.java

--- src/mmonly/MMOnlyData.java
+++ src/mmonly/MMOnlyData.java
@@ -19,6 +19,8 @@ public class MMOnlyData {
             "http://www.mmonly.cc/mmtp/xgmn/212622.html", "http://www.mmonly.cc/mmtp/xgmn/212080.html", "http://www.mmonly.cc/mmtp/hgmn/213663.html",
             "http://www.mmonly.cc/mmtp/xgmn/136957.html", "http://www.mmonly.cc/mmtp/hgmn/212577.html", "http://www.mmonly.cc/mmtp/qcmn/5688.html",
             "http://www.mmonly.cc/mmtp/xgmn/212611.html", "http://www.mmonly.cc/mmtp/swmn/213658.html","http://www.mmonly.cc/mmtp/hgmn/198665.html",
-            "http://www.mmonly.cc/mmtp/hgmn/4371.html","http://www.mmonly.cc/mmtp/hgmn/184850.html","http://www.mmonly.cc/mmtp/hgmn/75398.html","http://www.mmonly.cc/mmtp/hgmn/174804.html"
+            "http://www.mmonly.cc/mmtp/hgmn/4371.html","http://www.mmonly.cc/mmtp/hgmn/184850.html","http://www.mmonly.cc/mmtp/hgmn/75398.html","http://www.mmonly.cc/mmtp/hgmn/174804.html",
+            "http://www.mmonly.cc/mmtp/hgmn/212589.html","http://www.mmonly.cc/mmtp/hgmn/134016.html","http://www.mmonly.cc/mmtp/hgmn/146335.html",
+            "http://www.mmonly.cc/mmtp/hgmn/3227.html","http://www.mmonly.cc/mmtp/hgmn/1214.html"
     };
 }
